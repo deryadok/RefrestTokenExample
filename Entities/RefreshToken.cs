859e6c83b6e3d961b7fd7be43ac32377aa7c93fa
Derya Dok <derya.dok@ogr.deu.edu.tr>
2020-07-04 16:29:49 +0300

    Add project files.

---
+++ Entities/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RefrestTokenExample.Entities
+{
+    [Owned]
+    public class RefreshToken
+    {
+        [Key]
+        [JsonIgnore]
+        public int Id { get; set; }
+
+        public string Token { get; set; }
+    }
+}
